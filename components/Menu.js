1139adbd940846d8efc00efc8699e427ae9e9755
ximena.paparella@karvi.com <ximenapaparella@gmail.com>
2022-09-19 14:50:13 -0300

    add styles to submenu

--- components/Menu.js
+++ components/Menu.js
@@ -3,19 +3,73 @@ import Link from "next/link";
 import styles from "../styles/Menu.module.css";
 
 const Menu = () => {
+  const hasSubemenu = () => {
+    console.log("submenu");
+  };
+
   return (
-    <nav className={styles.nav}>
-      <Link href="/">Inicio</Link>
-      <Link href="/acerca-de">Acerca de</Link>
-      <Link href="/catalogo">Catalogo</Link>
-      <Link href="/servicios">Servicios</Link>
-      <Link href="/industria">Industria</Link>
-      <Link href="/normativa">Normativa</Link>
-      <Link href="/preguntas-frecuentes">Preguntas Frecuentes</Link>
-      <Link href="/formulario-de-ingreso">Formulario de Ingreso</Link>
-      <Link href="/contacto">Contacto</Link>
-      <Link href="/usuario">Usuario</Link>
-    </nav>
+    <ul className={styles.nav}>
+      <li>
+        <Link href="/">Inicio</Link>
+      </li>
+      <li>
+        <Link href="/acerca-de">Acerca de</Link>
+      </li>
+      <li>
+        <Link href="/catalogo">Catalogo</Link>
+      </li>
+      <li className={styles.hasSubmenu}>
+        <Link href="/servicios">Servicios</Link>
+        <ul className={styles.submenu}>
+          <li className={styles["submenu-item"]}>
+            <Link href="/servicios/provision-de-material">
+              Provisión de material
+            </Link>
+          </li>
+          <li>
+            <Link href="/servicios/preservacion-de-material">
+              Preservacion de Material
+            </Link>
+          </li>
+          <li>
+            <Link href="/servicios//capacitaciones">Capacitaciones</Link>
+          </li>
+          <li>
+            <Link href="/servicios/asesoria">Asesoria</Link>
+          </li>
+          <li>
+            <Link href="/servicios/otros">Otros</Link>
+          </li>
+        </ul>
+      </li>
+      <li>
+        <Link href="/industria">Industria</Link>
+      </li>
+      <li>
+        <Link href="/normativa">Normativa</Link>
+      </li>
+      <li>
+        <Link href="/preguntas-frecuentes">Preguntas Frecuentes</Link>
+      </li>
+      <li>
+        {" "}
+        <Link href="/formulario-de-ingreso">Formulario de Ingreso</Link>
+      </li>
+      <li>
+        <Link href="/contacto">Contacto</Link>
+      </li>
+      <li className={styles.hasSubmenu}>
+        <Link href="/usuario">Usuario</Link>
+        <ul className={styles.submenu}>
+          <li className={styles["submenu-item"]}>
+            <Link href="/iniciar-sesion">Iniciar sesión</Link>
+          </li>
+          <li>
+            <Link href="/registro">Registro de usuario</Link>
+          </li>
+        </ul>
+      </li>
+    </ul>
   );
 };
 
