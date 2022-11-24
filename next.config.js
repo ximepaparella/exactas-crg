fb38b3675ba04522076265a90b2430f89e6f35d1
ximena.paparella@karvi.com <ximenapaparella@gmail.com>
2022-11-24 01:23:07 -0300

    disable link next error

--- next.config.js
+++ next.config.js
@@ -2,6 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  extends: "next",
+  rules: {
+    "react/no-unescaped-entities": "off",
+    "@next/next/no-page-custom-font": "off",
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
